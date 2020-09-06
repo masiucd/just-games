faab38f92c78612b5a63c226a5437a4258e4c992
Marcell Ciszek Druzynski <40737996+masiuciszek@users.noreply.github.com>
2020-09-06 10:58:50 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,54 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
+// fn main() {
+//     println!("guess a number");
+//     let secret_number = rand::thread_rng().gen_range(1, 101);
+
+//     loop {
+//         println!("input your  guess");
+
+//         let mut guess = String::new();
+
+//         io::stdin()
+//             .read_line(&mut guess)
+//             .expect("failes to read line");
+
+//         let guess: u32 = guess.trim().parse().expect("please type a number!");
+
+//         println!("You guessed : {} ", guess);
+
+//         match guess.cmp(&secret_number) {
+//             Ordering::Less => println!("Too small"),
+//             Ordering::Greater => println!("Too big"),
+//             Ordering::Equal => {
+//                 println!("you guessed correct!!! with number of {} ", secret_number);
+//                 break;
+//             }
+//         }
+//     }
+// }
+
+fn sum(a: u32, b: u32) -> u32 {
+    a + b
+}
+
 fn main() {
-    println!("Guess the number!");
+    println!("enter a first number  ");
 
-    println!("Please input your guess.");
+    let mut first_num = String::new();
+    io::stdin().read_line(&mut first_num);
 
-    let mut guess = String::new();
+    println!("enter the second number");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    let mut second_num = String::new();
+    io::stdin().read_line(&mut second_num);
 
-    println!("You guessed: {}", guess);
+    let a: u32 = first_num.trim().parse().expect("NaN number");
+    let b: u32 = second_num.trim().parse().expect("NaN number");
+
+    let res = sum(a, b);
+
+    println!("result {} + {} = {} ", first_num, second_num, res);
 }
