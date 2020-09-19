df2176b1a43acd73cdf71659b9b1b27b8a4aefd4
Masiu <ciszekmarcell@gmail.com>
2020-09-19 10:57:18 +0200

    ownership

--- src/main.rs
+++ src/main.rs
@@ -4,114 +4,36 @@ use std::io;
 use std::process;
 
 fn main() {
-    println!("guess a number");
-    let secret = rand::thread_rng().gen_range(1, 101);
+    let s = String::from("Legia");
 
-    loop {
-        println!("give me a number");
-        let mut guess = String::new();
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(err) => continue,
-        };
-        println!("your guess {} ", guess);
+    // take_ownership(s);
 
-        match guess.cmp(&secret) {
-            Ordering::Less => println!("To low"),
-            Ordering::Greater => println!("To Big"),
-            Ordering::Equal => {
-                println!("You win the game! {} ", secret);
-                break;
-            }
-        }
-    }
+    // takes_and_gives_back(s);
+
+    let s_len = calc_length(s);
+
+    println!("{:?}", s_len);
+
+    let x = 24;
+
+    make_copy(x);
+
+    println!("{}", x);
 }
 
-// fn main() {
-//     println!("guess a number");
-//     let secret_number = rand::thread_rng().gen_range(1, 101);
-
-//     loop {
-//         println!("input your  guess");
-
-//         let mut guess = String::new();
-
-//         io::stdin()
-//             .read_line(&mut guess)
-//             .expect("failes to read line");
-
-//         let guess: u32 = guess.trim().parse().expect("please type a number!");
-
-//         println!("You guessed : {} ", guess);
-
-//         match guess.cmp(&secret_number) {
-//             Ordering::Less => println!("Too small"),
-//             Ordering::Greater => println!("Too big"),
-//             Ordering::Equal => {
-//                 println!("you guessed correct!!! with number of {} ", secret_number);
-//                 break;
-//             }
-//         }
-//     }
-// }
-// fn main() {
-//     let secret_number = rand::thread_rng().gen_range(1, 101);
-//     loop {
-//         println!("Please enter a first number: ");
-//         let a = read_user_input();
-
-//         println!("Please enter a second number: ");
-//         let b = read_user_input();
-
-//         let result = sum(a, b);
-//         println!("{} + {} = {}", a, b, result);
-
-//         match result.cmp(&secret_number) {
-//             Ordering::Less => println!("to small"),
-//             Ordering::Greater => println!("to large"),
-//             Ordering::Equal => {
-//                 println!("correct");
-//                 break;
-//             }
-//         }
-//     }
-// }
-
-fn sum(a: u32, b: u32) -> u32 {
-    a + b
+fn take_ownership(random_string: String) {
+    println!("{}", random_string);
 }
 
-fn read_user_input() -> u32 {
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-
-    let digit: u32;
-
-    match input.trim().parse() {
-        Ok(val) => digit = val,
-        Err(_err) => {
-            println!("Not a valid number!");
-            process::exit(1);
-        }
-    };
-
-    digit
+fn takes_and_gives_back(a_string: String) -> String {
+    a_string
 }
 
-fn fac(num: u32) -> u32 {
-    if num <= 2 {
-        num
-    } else {
-        num * fac(num - 1)
-    }
+fn make_copy(some_int: u32) {
+    println!("{} ", some_int);
 }
 
-fn facWithMatch(num: u32) -> u32 {
-    match num {
-        1 | 2 => num,
-        _ => num * fac(num - 1),
-    }
+fn calc_length(s: String) -> (String, usize) {
+    let len = s.len();
+    (s, len)
 }
