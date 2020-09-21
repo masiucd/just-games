526faf597f24a93e739183cb5f14045e2d51ff90
Masiu <ciszekmarcell@gmail.com>
2020-09-21 20:00:41 +0200

    uppercase letter

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
 fn main() {
-    println!("{}", upper_letter("legia"));
-    println!("{}", upper_letter("warszawa"));
-    println!("{}", upper_letter("boris"));
-}
+    let s = String::from("hello");
+    let len = s.len();
 
-fn upper_letter(s: &str) -> String {
-    let mut c = s.chars();
-    match c.next() {
-        None => String::new(),
-        Some(f) => f.to_uppercase().chain(c).collect(),
-    }
+    let slice = &s[0..len];
+    println!("{}", slice);
 }
