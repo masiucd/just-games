8761a684febd77efb2ec886adbe20d0836173eba
Masiu <ciszekmarcell@gmail.com>
2020-10-31 18:37:00 +0100

    structs

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,29 @@
-fn main() {
-    let collected_iterator: Vec<i32> = (0..10).collect();
-    // println!("Collected (0..10) into: {:?}", collected_iterator);
-
-    let mut xs = vec![1i32, 2, 3];
-    // println!("Initial vector: {:?}", xs);
-
-    for v in collected_iterator {
-        // println!("v is  {} ", v);
-    }
-
-    println!("insert new item into xs");
-    let four_teen = 14;
-    xs.push(four_teen);
-    println!("now xs has {} as well  {:?}", four_teen, xs);
-
-    let xsLen = get_len(&xs); // we need to barrow the xs vector, make a reference
-    println!("xs length is : {:?}", xsLen);
-
-    for x in xs.iter() {
-        println!("x > {}", x);
-    }
+#[derive(Debug)]
+struct Person {
+    name: String,
+    age: u8,
 }
-// make a reference to vector xs
-fn get_len(xs: &Vec<i32>) -> usize {
-    xs.len()
+
+struct Pair(i32, i32);
+
+fn main() {
+    let bobby = Person {
+        name: String::from("Bobby"),
+        age: 34,
+    };
+
+    println!(
+        "bobby is a struct: age of  {} and name {}",
+        bobby.age, bobby.name
+    );
+
+    let pair = Pair(33, 23);
+    println!("pair {:#?}", pair.0);
+
+    let res = rect_area(&pair);
+    println!("area would be : {}", res);
+}
+
+fn rect_area(pair: &Pair) -> i32 {
+    pair.0 * pair.1
 }
