2c738d2087f6da3f7baf0a8c4310e0aa8e22eaa4
masiucd <ciszekmarcell@gmail.com>
2021-08-02 07:56:33 +0200

    title

--- components/app/header.tsx
+++ components/app/header.tsx
@@ -1,6 +1,24 @@
 import {css} from '@emotion/react'
+import styled from '@emotion/styled'
 import {sizes} from '@styles/styled-record'
 import Navbar from './navbar'
+import cuid from 'cuid'
+import {motion} from 'framer-motion'
+
+const toSpans = (input: string) =>
+  input.split('').map((letter) => (
+    <motion.span
+      whileHover={{scale: 1.1, rotateX: 20, rotateY: 30, y: -2}}
+      key={cuid()}
+      css={css`
+        display: inline-block;
+      `}
+    >
+      {letter}
+    </motion.span>
+  ))
+
+const HeaderTitle = styled.div``
 
 const Header = () => {
   return (
@@ -9,7 +27,10 @@ const Header = () => {
         height: ${sizes.headerHeight};
       `}
     >
-      <h1>Header</h1>
+      <HeaderTitle>
+        <strong>{toSpans('Just Games')}</strong>
+      </HeaderTitle>
+
       <Navbar />
     </header>
   )
