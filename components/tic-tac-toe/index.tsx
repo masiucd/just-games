a615a34030480e7b5737ab55834056b0c9b201bb
masiucd <ciszekmarcell@gmail.com>
2021-08-06 12:55:01 +0200

    tic tac toe with useState

--- components/tic-tac-toe/index.tsx
+++ components/tic-tac-toe/index.tsx
@@ -3,7 +3,7 @@ import {resetButtonStyles} from "@styles/common"
 import {colors, elevations} from "@styles/styled-record"
 import {checkWinner} from "@utils/check-winner"
 import cuid from "cuid"
-import {Fragment, useEffect, useState} from "react"
+import {Fragment, useEffect, useReducer} from "react"
 
 const Grid = styled.section`
   display: grid;
@@ -28,37 +28,102 @@ const Grid = styled.section`
   }
 `
 
-export const TicTacToe = (): JSX.Element => {
-  const [squares, setSquares] = useState(Array(9).fill(null))
-  const [isX, setIsX] = useState(false)
-  const [winner, setWinner] = useState<null | string>(null)
+interface State {
+  squares: Array<null | string>
+  isX: boolean
+  winner: null | string
+  score: {
+    oScore: number
+    xScore: number
+  }
+}
 
-  const winnerSymbol = checkWinner(squares)
+type Action =
+  | {type: "SET_SQUARE"; newSquares: Array<string | null>}
+  | {type: "SET_WINNING_SYMBOL"; winningSymbol: string}
+  | {type: "NEW_ROUND"}
+  | {type: "RESET_GAME"}
+
+function reducer(state: State, action: Action) {
+  switch (action.type) {
+    case "SET_SQUARE":
+      return {
+        ...state,
+        isX: !state.isX,
+        squares: action.newSquares,
+      }
+
+    case "SET_WINNING_SYMBOL":
+      return {
+        ...state,
+        winner: action.winningSymbol,
+        score: {
+          ...state.score,
+          oScore:
+            action.winningSymbol === "O"
+              ? state.score.oScore + 1
+              : state.score.oScore,
+          xScore:
+            action.winningSymbol === "X"
+              ? state.score.xScore + 1
+              : state.score.xScore,
+        },
+      }
+
+    case "NEW_ROUND":
+      return {
+        ...state,
+        squares: Array(9).fill(null),
+        isX: false,
+        winner: null,
+      }
+
+    default:
+      throw new Error(`action.type does not exists`)
+  }
+}
+
+const initialState: State = {
+  squares: Array(9).fill(null),
+  isX: false,
+  winner: null,
+  score: {
+    oScore: 0,
+    xScore: 0,
+  },
+}
+
+export const TicTacToe = (): JSX.Element => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  const winningSymbol = checkWinner(state.squares)
 
   const handleClick = (index: number): void => {
-    if (squares[index] || winner) {
+    if (state.squares[index] || state.winner) {
       return
     }
-    setSquares((prev) => {
-      const xs = [...prev]
-      xs[index] = isX ? "X" : "O"
-      return xs
-    })
-    setIsX((prev) => !prev)
+    const newSquares = [...state.squares]
+    newSquares[index] = state.isX ? "X" : "O"
+    dispatch({type: "SET_SQUARE", newSquares})
   }
 
   useEffect(() => {
-    if (winnerSymbol !== null) {
-      setWinner(winnerSymbol)
+    if (winningSymbol !== null) {
+      dispatch({type: "SET_WINNING_SYMBOL", winningSymbol})
     }
-  }, [winnerSymbol])
+  }, [winningSymbol])
 
   return (
     <Fragment>
+      <h3>
+        O Score :{state.score.oScore} <br /> X Score: {state.score.xScore}
+      </h3>
       <Grid>
-        {squares.map((square, index) => (
+        {state.squares.map((square, index) => (
           <button
-            onClick={(): void => handleClick(index)}
+            onClick={() => {
+              handleClick(index)
+            }}
             key={cuid()}
             className={`square square${index}`}
           >
