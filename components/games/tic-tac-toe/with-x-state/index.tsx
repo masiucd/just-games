26fc19f0e4d975ffd11d6960444c444fa43baa7c
masiucd <ciszekmarcell@gmail.com>
2021-08-14 14:24:51 +0200

    with xstate2.0

--- components/games/tic-tac-toe/with-x-state/index.tsx
+++ components/games/tic-tac-toe/with-x-state/index.tsx
@@ -3,18 +3,43 @@ import {css} from "@emotion/react"
 import styled from "@emotion/styled"
 import {resetButtonStyles} from "@styles/common"
 import {colors} from "@styles/styled-record"
+import {checkWinner} from "@utils/check-winner"
 import {makeList} from "@utils/helpers"
 import {useMachine} from "@xstate/react"
 import cuid from "cuid"
 import {motion} from "framer-motion"
-import {createMachine} from "xstate"
+import {useEffect} from "react"
+import {assign, createMachine} from "xstate"
 
 const AMOUNT_OF_SQUARES = 9
 
 const START_GAME = "START_GAME"
 const SELECT_SQUARE = "SELECT_SQUARE"
+const SET_WINNER = "SET_WINNER"
 
-const ticTacToeMachine = createMachine({
+type Player = "X" | "O"
+
+interface TicTacToeCtx {
+  squares: Array<Player | null>
+  isX: boolean
+  winner: Player | null
+  currentGameSet: number
+  amountOfGameSets: number
+  finalWinner: null | Player
+  isOptionsDialogOpen: boolean
+  isDraw: boolean
+  score: {
+    oScore: number
+    xScore: number
+  }
+}
+
+type TicTacToeEvents =
+  | {type: "START_GAME"}
+  | {type: "SELECT_SQUARE"; index: number}
+  | {type: "SET_WINNER"}
+
+const ticTacToeMachine = createMachine<TicTacToeCtx, TicTacToeEvents>({
   id: "idle",
   initial: "idle",
   context: {
@@ -35,19 +60,34 @@ const ticTacToeMachine = createMachine({
     idle: {
       on: {
         [START_GAME]: {
-          target: "playing",
+          target: "start",
         },
       },
     },
-    playing: {
-      id: "play",
-      on: {
-        [SELECT_SQUARE]: {
-          // actions: () => {}
-          actions: "selectSquare",
-        },
-      },
+    start: {
+      id: "starting",
+      initial: "playing",
       states: {
+        playing: {
+          on: {
+            [SELECT_SQUARE]: {
+              actions: assign({
+                squares: ({squares, isX}, {index}) => {
+                  if (squares[index] || checkWinner(squares)) return squares
+                  const newSquaresList = [...squares]
+                  newSquaresList[index] = isX ? "X" : "O"
+                  return newSquaresList
+                },
+                isX: ({isX}) => {
+                  return !isX
+                },
+              }),
+            },
+            [SET_WINNER]: {
+              target: "#starting.winning",
+            },
+          },
+        },
         winning: {},
         loosing: {},
         draw: {},
@@ -64,6 +104,7 @@ const squareStyles = css`
   min-width: 10rem;
   min-height: 10rem;
   background-color: ${colors.colorHighlight};
+  font-size: 2em;
 `
 const Grid = styled(motion.section)`
   margin: 0 auto;
@@ -77,18 +118,26 @@ const Grid = styled(motion.section)`
 `
 
 const TicTacToeWithXState = () => {
-  const [value, send] = useMachine(ticTacToeMachine)
+  const [state, send] = useMachine(ticTacToeMachine)
+  const {squares} = state.context
+  const isIdle = state.matches("idle")
+  const hasStart = state.matches("start")
 
-  const {squares} = value.context
-  const isIdle = value.matches("idle")
-  const isPlaying = value.matches("playing")
+  useEffect(() => {
+    if (hasStart && checkWinner(squares)) {
+      send({type: "SET_WINNER"})
+      console.log("WINNER")
+    }
+  }, [hasStart, send, squares])
+
+  console.log(state.value)
   return (
     <GameWrapper>
       {isIdle && (
         <button onClick={() => send({type: "START_GAME"})}>start</button>
       )}
 
-      <AnimatedWrapper isOn={isPlaying}>
+      <AnimatedWrapper isOn={hasStart}>
         <Grid
           initial={{opacity: 0, scale: 0.65}}
           animate={{opacity: 1, scale: 1}}
@@ -98,8 +147,13 @@ const TicTacToeWithXState = () => {
             damping: 6,
           }}
         >
-          {squares.map((square) => (
-            <button key={cuid()}>{square}</button>
+          {squares.map((square, index) => (
+            <button
+              key={cuid()}
+              onClick={() => send({type: "SELECT_SQUARE", index})}
+            >
+              {square}
+            </button>
           ))}
         </Grid>
       </AnimatedWrapper>
